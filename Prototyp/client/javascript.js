e331ed4a8b45080493bb1f041bf189800b213216
Andreas Fitz OmgUsername <iwttst@freenet.de>
2014-05-11 20:48:12 +0200

    - Kollisionserkennung auf der Plane/Boden (Jede Kollision wir vom "Counter" mitgezählt. Zurücksetzen auf die letzte bekannte Position noch nicht vorhanden)

--- Prototyp/client/javascript.js
+++ Prototyp/client/javascript.js
@@ -1,76 +1,66 @@
-window.onload = function() {
-    var camera = document.getElementById("camera");
-    var control = document.getElementById("control");
-    var pointer = document.getElementById("pointer");
-    var activated = false;
-    var position = {x: 0, y: 0};
 
-    control.addEventListener(
-        "mousedown", 
-        function(event){
-            activated = true;
-            position.x = event.layerX;
-            position.y = event.layerY;
-            pointer.style.left = event.layerX + "px";
-            pointer.style.top = event.layerY + "px";
-        }, 
-        true);
 
-    control.addEventListener(
-        "mouseleave", 
-        function(event){
-            activated = false;
-            position.x = 0;
-            position.y = 0;
-            pointer.style.left = "100px";
-            pointer.style.top = "100px";
-            event.stopPropagation();
-        }, 
-        true);
+var dragobjekt = null;
+var dragx = 0;
+var dragy = 0;
+var posx = 0;
+var posy = 0;
+var start = null;
 
-    control.addEventListener(
-        "mouseup", 
-        function(event){
-            activated = false;
-            position.x = 0;
-            position.y = 0;
-            pointer.style.left = "100px";
-            pointer.style.top = "100px";
-            event.stopPropagation();
-        }, 
-        true);
 
-    control.addEventListener(
-        "mousemove", 
-        function(event){
-            if (activated) {
-                console.log(event.target.parentNode.offsetLeft + " " + event.target.parentNode.offsetTop);
-                position.x = event.layerX;
-                position.y = event.layerY;
-                pointer.style.left = (event.layerX - 25) + "px";
-                pointer.style.top = (event.layerY - 25) + "px";
-                event.stopPropagation();
-            }
-        }, 
-        true);
+function draginit(){
+    
 
-    function tick() {
-        if (activated) {
-            var currentPosition = x3dom.fields.SFVec3f.parse(camera.getAttribute("position"));
-            var halfWidth = (control.clientWidth / 2.0);
-            var halfHeight = (control.clientHeight / 2.0);
-            var facX = -(1.0 / halfWidth * (-halfWidth + position.x));
-            var facY = 1.0 / halfHeight * (-halfHeight + position.y);
-            currentPosition.x += facX;
-            currentPosition.y += facY;
-            camera.setAttribute("position", currentPosition.toString());
-        }
+    document.onmousemove = drag;
+    document.onmouseup = dragstop;
+    
+    
+}
 
-        window.requestAnimationFrame(tick);
-    };
+function coords(){
+    
+    start = window.event.x && window.event.y;
+    dragobjekt = start;
+    
+}
 
-    window.requestAnimationFrame(tick);
-};
+
+function dragstart(element){
+    
+    dragobjekt = element;
+    dragx = posx - dragobjekt.offsetLeft;
+    dragy = posy - dragobjekt.offsetTop;
+    
+}
+
+
+
+function drag(ereignis){
+    
+     posx = document.onclick ? window.event.clientX : ereignis.pageX;
+     posy = document.onclick ? window.event.clientY : ereignis.pageY;
+        
+    if(dragobjekt !== null) {
+        dragobjekt.style.left = (posx - dragx) + "px";
+        dragobjekt.style.top = (posy - dragy) + "px";
+    }
+    
+}
+
+function dragstop(e){
+    
+    
+    posx = document.onclick ? window.event.clientX : e.pageX;
+    posy = document.onclick ? window.event.clientY : e.pageY;
+    
+    if(dragobjekt !== null){
+        
+        dragobjekt = posx;
+        dragobjekt = posy;
+    }
+    
+    
+}
 
 function switchCamera(id){
 	var vp = document.getElementById(id);
@@ -78,17 +68,14 @@ function switchCamera(id){
 	
 }
 
-function changeSpeed(){	
-	
+function changeSpeed(){
 	var x = document.getElementById("x3d_element");
-		
 	var y = document.changeSpeed.speed.value;
 		if(y===null)
 		{
 			y=20;
 		}
 	x.runtime.speed(y);
-
 }
 
 function lookArround(){
