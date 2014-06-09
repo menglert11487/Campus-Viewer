4197920ebcbc937d8f099adb5ae58454873a2167
TigerLily91 <desiree.nm@web.de>
2014-06-09 14:48:47 +0200

    Update des Buttons

--- Prototyp/client/loginout.js
+++ Prototyp/client/loginout.js
@@ -1,16 +1,30 @@
-			var loginoutvar = 0;
+				var loginoutvar = 0;
 			
   			function loginOrOut()
 			{
 				if (loginoutvar == 0)
 				{
 					document.getElementById('loginout').innerHTML = "Logout";
-					document.getElementById('loginout').href = "login.html";
 					loginoutvar = 1;
+					window.open("login.html");
 
 				} else if (loginoutvar == 1)
 				{
 					document.getElementById('loginout').innerHTML = "Login";
 					loginoutvar = 0;
+					
 				}				
 			}
+			
+			
+var foo = function(){
+  var button = document.createElement('button');
+  button.innerHTML = 'click me';
+  button.onclick = function(){
+    alert('here be dragons');return false;
+  };
+  // where do we want to have the button to appear?
+  // you can append it to another element just by doing something like
+  // document.getElementById('foobutton').appendChild(button);
+  document.body.appendChild(button);
+};
